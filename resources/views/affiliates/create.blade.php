5f6b17e53ff5e0d58daa4c1006ce25828b2c9624
Luke <luke.watts@workvivo.com>
2023-12-06 18:12:17 +0000

    List affiliates view affiliates/create.blade.php

--- resources/views/affiliates/create.blade.php
+++ resources/views/affiliates/create.blade.php
@@ -9,13 +9,33 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    @dd($affiliates)
-                    @foreach ($affiliates as $affiliate)
-                        <dl>
-                            <dt class="text-lg font-bold">
-                                {{ $affiliate->name }}
-                            </dt>
-                    @endforeach
+                    <ul>
+                        @foreach ($affiliates as $affiliate)
+                            <li class="flex flex-row max-w-[250px] justify-between">
+                                <span class="text-xl font-bold">ID:</span>
+                                <span class="text-lg">{{ $affiliate->affiliate_id }}</span>
+                            </li>
+                            <li class="flex flex-row max-w-[250px] justify-between">
+                                <span class="text-xl font-bold">Name:</span>
+                                <span class="text-lg">{{ $affiliate->name }}</span>
+                            </li>
+                            <li class="flex flex-row max-w-[250px] justify-between">
+                                <span class="text-xl font-bold">Latitude:</span>
+                                <span class="text-lg">{{ $affiliate->latitude }}</span>
+                            </li>
+                            <li class="flex flex-row max-w-[250px] justify-between">
+                                <span class="text-xl font-bold">Longitude:</span>
+                                <span class="text-lg">{{ $affiliate->longitude }}</span>
+                            </li>
+                            <li class="flex flex-row max-w-[250px] justify-between">
+                                <span class="text-xl font-bold">Distance:</span>
+                                <span class="text-lg">{{ $affiliate->distance }}</span>
+                            </li>
+                            @if (!$loop->last)
+                                <hr class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700">
+                            @endif
+                        @endforeach
+                    </ul>
                 </div>
             </div>
         </div>
