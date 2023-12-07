63e6bd680f707612da94e0509985fd6ccf6b4a4d
Luke <luke.watts@workvivo.com>
2023-12-07 00:06:58 +0000

    Routing and middleware usage improvements

--- routes/auth.php
+++ routes/auth.php
@@ -1,15 +1,14 @@
 <?php
 
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
-use App\Http\Controllers\Auth\ConfirmablePasswordController;
 use App\Http\Controllers\Auth\EmailVerificationNotificationController;
 use App\Http\Controllers\Auth\EmailVerificationPromptController;
-use App\Http\Controllers\Auth\NewPasswordController;
-use App\Http\Controllers\Auth\PasswordController;
-use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
+use Laravel\Socialite\Facades\Socialite;
+use Laravel\Socialite\Two\InvalidStateException;
 
 Route::middleware('guest')->group(function () {
     Route::get('register', [RegisteredUserController::class, 'create'])
@@ -21,6 +20,50 @@ Route::middleware('guest')->group(function () {
                 ->name('login');
 
     Route::post('login', [AuthenticatedSessionController::class, 'store']);
+});
+
+Route::get('/auth/redirect', function () {
+    return Socialite::driver('github')->redirect();
+});
+
+Route::get('/auth/callback', function () {
+    if (auth()->check()) {
+        return redirect()->intended('/dashboard');
+    }
+
+    $user = Socialite::driver('github')->user();
+
+    if (!$user) {
+        throw new InvalidStateException('Unable to authenticate with GitHub.');
+    }
+
+    // Check if user with email exists
+    $existingUser = User::where('email', $user->getEmail())->first();
+    if ($existingUser) {
+        // Update user's GitHub details
+        $existingUser->github_id = $user->getId();
+        $existingUser->github_token = $user->token;
+        $existingUser->github_refresh_token = $user->refreshToken;
+        $existingUser->save();
+
+        // Login the user
+        auth()->login($existingUser, true);
+
+        return redirect()->intended('/dashboard');
+    }
+
+    $newUser = User::create([
+        'name' => $user->getName(),
+        'email' => $user->getEmail(),
+        'email_verified_at' => now(),
+        'github_id' => $user->getId(),
+        'github_token' => $user->token,
+        'github_refresh_token' => $user->refreshToken,
+    ]);
+
+    auth()->login($newUser, true);
+
+    return redirect()->intended('/dashboard');
 });
 
 Route::middleware('auth')->group(function () {
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,7 @@
 <?php
 
 use App\Http\Controllers\AffiliatesController;
-use App\Http\Controllers\ProfileController;
-use App\Models\User;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
-use Laravel\Socialite\Facades\Socialite;
-use Laravel\Socialite\Two\InvalidStateException;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,56 +18,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::prefix('dashboard')->middleware(['auth', 'verified'])->group(function () {
+    Route::get('/', function () {
+        return view('dashboard');
+    })->name('dashboard');
 
-Route::get('/auth/redirect', function () {
-    return Socialite::driver('github')->redirect();
-});
-
-Route::get('/auth/callback', function () {
-    if (auth()->check()) {
-        return redirect()->intended('/dashboard');
-    }
-
-    $user = Socialite::driver('github')->user();
-
-    if (!$user) {
-        throw new InvalidStateException('Unable to authenticate with GitHub.');
-    }
-
-    // Check if user with email exists
-    $existingUser = User::where('email', $user->getEmail())->first();
-    if ($existingUser) {
-        // Update user's GitHub details
-        $existingUser->github_id = $user->getId();
-        $existingUser->github_token = $user->token;
-        $existingUser->github_refresh_token = $user->refreshToken;
-        $existingUser->save();
-
-        // Login the user
-        auth()->login($existingUser, true);
-
-        return redirect()->intended('/dashboard');
-    }
-
-    $newUser = User::create([
-        'name' => $user->getName(),
-        'email' => $user->getEmail(),
-        'email_verified_at' => now(),
-        'github_id' => $user->getId(),
-        'github_token' => $user->token,
-        'github_refresh_token' => $user->refreshToken,
-    ]);
-
-    auth()->login($newUser, true);
-
-    return redirect()->intended('/dashboard');
-});
-
-Route::prefix('dashboard')->group(function () {
     Route::get('/list-affiliates', [AffiliatesController::class, 'create'])->name('dashboard.list-affiliates');
-})->middleware(['auth', 'verified']);
+});
 
 require __DIR__.'/auth.php';
